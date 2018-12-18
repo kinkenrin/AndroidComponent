ff5e08d9e780c1b004d4ad163f7c125d46fe975d
jinxl <121552497@qq.com>
2018-12-18 13:43:41 +0800

    初步完成home组件

---
+++ component_home/src/main/runalone/java/com/github/king/androidcomponent/home_/MainActivity.java
@@ -0,0 +1,15 @@
+package com.github.king.androidcomponent.home_;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.github.king.home.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_activity_main);
+    }
+}
