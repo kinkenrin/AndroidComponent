207ec65deafd81b1c11c05452a3bed1fcf6487a9
jinxianlun <121552497@qq.com>
2019-08-22 18:19:54 +0800

    提交代码

--- component_service/src/main/java/com/github/king/componentservice/home/HomeService.java
+++ component_service/src/main/java/com/github/king/componentservice/home/HomeService.java
@@ -6,10 +6,10 @@ import com.github.king.basiclib.base.IComponentService;
 
 /**
  * compenent_home 对外提供服务的接口
+ * 声明接口,其他组件通过接口来调用服务
  *
  * @author Created by jinxl on 2018/12/20.
  */
-// 声明接口,其他组件通过接口来调用服务
 public interface HomeService extends IComponentService {
     //访问路径
     String PATH_SHAREBOOK = "/home/shareBook";
